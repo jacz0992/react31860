c66249adb03488c3bdb732d576eac76cf09ecf49
jacz0992 <jacz0992@gmail.com>
2022-04-14 22:25:09 -0500

    react81860

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HOLA MUNDO!, ESTO ES UNA TIENDA DE PAÑALES.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
